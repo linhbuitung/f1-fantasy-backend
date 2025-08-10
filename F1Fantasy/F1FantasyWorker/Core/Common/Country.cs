d07e2caa3c99e1cf29387713501e95da400a838a
= <=>
2025-08-10 11:16:23 +0200

    Extension

---
+++ F1Fantasy/F1FantasyWorker/Core/Common/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace F1FantasyWorker.Core.Common;
+
+public partial class Country
+{
+    public string Id { get; set; } = null!;
+
+    public string ShortName { get; set; } = null!;
+
+    public List<string> Nationalities { get; set; } = null!;
+
+    public virtual ICollection<AspNetUser> AspNetUsers { get; set; } = new List<AspNetUser>();
+
+    public virtual ICollection<Circuit> Circuits { get; set; } = new List<Circuit>();
+
+    public virtual ICollection<Constructor> Constructors { get; set; } = new List<Constructor>();
+
+    public virtual ICollection<Driver> Drivers { get; set; } = new List<Driver>();
+}
---
+++ F1Fantasy/F1FantasyWorker/Modules/StaticDataModule/Services/Implementations/CountryService.cs
@@ -0,0 +1,110 @@
+﻿using F1FantasyWorker.Core.Common;
+using F1FantasyWorker.Infrastructure.Contexts;
+using F1FantasyWorker.Modules.StaticDataModule.Dtos;
+using F1FantasyWorker.Modules.StaticDataModule.Dtos.Mapper;
+using F1FantasyWorker.Modules.StaticDataModule.Repositories.Interfaces;
+using F1FantasyWorker.Modules.StaticDataModule.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1FantasyWorker.Modules.StaticDataModule.Services.Implementations
+{
+    public class CountryService : ICountryService
+    {
+        private readonly IStaticDataRepository _staticDataRepository;
+        private readonly WooF1Context _context;
+
+        public CountryService(IStaticDataRepository staticDataRepository, WooF1Context context)
+        {
+            _staticDataRepository = staticDataRepository;
+            _context = context;
+        }
+
+        public async Task<CountryDto> AddCountryAsync(CountryDto countryDto)
+        {
+            using var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                Country existingCountry = await _staticDataRepository.GetCountryByIdAsync(countryDto.CountryId);
+                if (existingCountry != null)
+                {
+                    return null;
+                }
+                // Replace special case for United States
+                countryDto = ReplaceSpecialCountryCase(countryDto);
+
+                Country country = StaticDataDtoMapper.MapDtoToCountry(countryDto);
+
+                Country newCountry = await _staticDataRepository.AddCountryAsync(country);
+
+                // Additional operations that need atomicity (example: logging the event)
+                await _context.SaveChangesAsync();
+
+                await transaction.CommitAsync();
+
+                return StaticDataDtoMapper.MapCountryToDto(newCountry);
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                Console.WriteLine($"Error creating country: {ex.Message}");
+
+                throw;
+            }
+        }
+
+        public async void AddListCountriesAsync(List<CountryDto> countryDtos)
+        {
+            foreach (var countryDto in countryDtos)
+            {
+                Console.WriteLine($"Adding country: {countryDto.CountryId}");
+                await AddCountryAsync(countryDto);
+            }
+        }
+
+        public async Task<CountryDto> GetCountryByIdAsync(string id)
+        {
+            Country country = await _staticDataRepository.GetCountryByIdAsync(id);
+            if (country == null)
+            {
+                return null;
+            }
+            return StaticDataDtoMapper.MapCountryToDto(country);
+        }
+
+        public async Task<CountryDto> GetCountryByNationalityAsync(string nationality)
+        {
+            Country country = await _staticDataRepository.GetCountryByNationalitityAsync(nationality);
+            if (country == null)
+            {
+                return null;
+            }
+            return StaticDataDtoMapper.MapCountryToDto(country);
+        }
+
+        public async Task<CountryDto> GetCountryByShortNameAsync(string shortName)
+        {
+            Country country = await _staticDataRepository.GetCountryByShortNameAsync(shortName);
+            if (country == null)
+            {
+                return null;
+            }
+            return StaticDataDtoMapper.MapCountryToDto(country);
+        }
+
+        private CountryDto ReplaceSpecialCountryCase(CountryDto countryDto)
+        {
+            if (countryDto.CountryId.Equals("United States"))
+            {
+                countryDto.CountryId = "USA";
+            }
+
+            return countryDto;
+        }
+    }
+}
